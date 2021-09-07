0dd060819346e7563565edd2e6b286693e7e1a0a
arf <goobysc@gmail.com>
2021-09-07 16:51:09 -0400

    checking if phone is alive

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from ciscoreset import PhoneConnection
+from time import sleep
+from ciscoreset import PhoneConnection, get_menu_position, get_list_position
 
 
-with PhoneConnection("10.12.4.231") as myphone:
-    myphone._to_reset_menu()
+with PhoneConnection("10.12.4.119") as myphone:
+    myphone.xml.send_key("Soft3")
+    sleep(7)
+    myphone._wait_until_reachable()
+    # myphone._screenshot()
+    print("DONE")
