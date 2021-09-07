e24e5cbe17692c4f08ac3d1d3dc1661dd44e1c8c
arf <goobysc@gmail.com>
2021-09-07 16:51:18 -0400

    added get_list_position

--- ciscoreset/__init__.py
+++ ciscoreset/__init__.py
@@ -2,6 +2,6 @@ __version__ = "0.1.0"
 from .axl import CUCM
 from .credentials import get_credentials
 from .keys import KEY_SUPPORT
-from .vision import get_menu_position
+from .vision import get_menu_position, get_list_position
 from .xml import XMLPhone
 from .phone import PhoneConnection
