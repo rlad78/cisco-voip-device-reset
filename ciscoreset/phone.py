1f69347dc8fb3deebf346c59fc0ae4efccec7a48
arf <goobysc@gmail.com>
2021-09-10 15:46:34 -0400

    interactive mode and verbose

--- ciscoreset/phone.py
+++ ciscoreset/phone.py
@@ -7,7 +7,7 @@ import requests
 import re
 from time import sleep
 from pathlib import Path
-import subprocess
+from icecream import ic
 
 SUPPORTED_PHONE_MODELS = [
     "8811",
@@ -38,7 +38,11 @@ RESET_CONFIRM_BUTTON: dict = {
 
 
 class PhoneConnection:
-    def __init__(self, phone_ip: str) -> None:
+    def __init__(self, phone_ip: str, verbose=False) -> None:
+        if not verbose:
+            ic.disable()
+        self.verbose = verbose
+
         self.username, self.password = get_credentials()
         self.device_ip = phone_ip
         self.ucm: CUCM = CUCM(self.username, self.password)
@@ -84,8 +88,15 @@ class PhoneConnection:
             print("Removing used device from admin profile...")
             self.ucm.update_user_devices(self.username, self.admin_devices)
 
-    def _screenshot(self) -> str:
+    def _screenshot(self, append="", full_name="") -> str:
         safe_ip_str = self.device_ip.replace(".", "-")
+        if append:
+            if not append.startswith("-"):
+                safe_ip_str += "-" + append
+            else:
+                safe_ip_str += append
+        elif full_name:
+            safe_ip_str = full_name
         return self.xml.download_screenshot(filepath=f"tmp/{safe_ip_str}.bmp")
 
     def _wait_until_reachable(self) -> None:
@@ -103,11 +114,14 @@ class PhoneConnection:
                 return None
 
     def __find_pos(self, item: str, f) -> int:
-        pos = f(item, self.device_model, self._screenshot())
+        if self.verbose:
+            screenshot = self._screenshot(append=item.lower().replace(" ", "-"))
+        else:
+            screenshot = self._screenshot()
+        pos = f(item, self.device_model, screenshot)
         if pos == -1:
             raise Exception(f"Cannot find {item}")
-        else:
-            return pos
+        return pos
 
     def _find_menu_pos(self, menu_item: str) -> int:
         return self.__find_pos(menu_item, get_menu_position)
@@ -124,40 +138,44 @@ class PhoneConnection:
     def _to_home(self) -> None:
         print("Navigating back to home menu... ", end="", flush=True)
         self.xml.send_keys(["NavBack"] * 6)
+        if self.verbose:
+            self._screenshot("-home")
         print("done")
 
-    def _to_settings(self) -> None:
+    def _to_applications(self, menu="") -> None:
         self._to_home()
-        print("Opening settings menu... ", end="", flush=True)
+        print("Opening applications... ", end="", flush=True)
         self.xml.send_key("Applications")
+        if self.verbose:
+            self._screenshot("-applications")
         print("done")
+        if menu:
+            print(f"Navigating to {menu} menu... ", end="", flush=True)
+            self._goto_menu_item(menu)
+            # if self.verbose:
+            #     self._screenshot(f"-{menu}-menu")
+            print("done")
 
     def _to_admin_settings_menu(self) -> None:
-        self._to_settings()
+        self._to_applications()
         print("Opening admin settings menu... ", end="", flush=True)
+        if self.verbose:
+            print("")
 
-        # admin_settings_pos = get_menu_position(
-        #     "Admin Settings", self.device_model, self._screenshot()
-        # )
-        # if admin_settings_pos == -1:
-        #     raise Exception("Cannot find Admin Settings menu")
-
-        # go to admin menu
         self.xml.send_key(f"KeyPad{self._find_menu_pos('Admin Settings')}")
+        # if self.verbose:
+        #     self._screenshot("-admin-settings")
         print("done")
 
     def _to_reset_settings_menu(self) -> None:
         self._to_admin_settings_menu()
         print("Opening reset menu... ", end="", flush=True)
+        if self.verbose:
+            print("")
 
-        # reset_settings_pos = get_list_position(
-        #     "Reset Settings", self.device_model, self._screenshot()
-        # )
-        # if reset_settings_pos == -1:
-        #     raise Exception("Cannot find Reset Settings menu")
-
-        # go to reset menu
         self.xml.send_key(f"KeyPad{self._find_list_pos('Reset Settings')}")
+        if self.verbose:
+            self._screenshot("-reset-settings")
         print("done")
 
     def send_reset(self, reset_type: str, dry_run=False) -> None:
@@ -176,13 +194,107 @@ class PhoneConnection:
 
         print(f"Sending {reset_type.title()} reset... ", end="", flush=True)
         self._goto_list_item(reset_commands[reset_type])
+        # if self.verbose:
+        # self._screenshot("-reset-select")
+
         if dry_run:
-            sleep(3)
+            print("dry-run complete")
+            sleep(1)
             self._to_home()
         else:
             self.xml.send_key(RESET_CONFIRM_BUTTON[self.device_model])
+            print("done")
             if reset_type in ["security"]:
                 self._to_home()
+                if self.verbose:
+                    self._screenshot("-finished-home")
             elif reset_type == "network":
                 self._wait_until_reachable()
-        print("done")
+
+    def interactive_mode(self) -> None:
+        self.verbose = False
+        print("Enter commands, either one at a time, or separated by commas.")
+        print(f"Make sure to be viewing {self._screenshot()} at the same time.")
+        print("([enter] a blank line to exit)\n")
+
+        def send_cmds(cmds):
+            if cmds:
+                self.xml.send_keys(cmds)
+            self._screenshot()
+
+        def set_font(fontsize: str) -> str:
+            sizes: dict = {
+                "1": "tiny",
+                "2": "small",
+                "3": "regular",
+                "4": "large",
+                "5": "huge",
+            }
+            if fontsize not in sizes:
+                print(f"Invalid font size of {fontsize}")
+                return "invalid"
+            else:
+                self._to_applications()
+                self._goto_menu_item("Settings")
+                print(f"Setting font size to {sizes[fontsize]}... ", end="", flush=True)
+                self.xml.send_keys(["KeyPad5", f"KeyPad{fontsize}", "Soft2"])
+                self._screenshot()
+                print("done")
+                return sizes[fontsize]
+
+        while (input_keys := input("-> ")) != "":
+            cmds: list[str] = replace_key_shortcuts(
+                input_keys.replace(",", " ").replace("  ", " ").split(" ")
+            )
+            if cmds == ["screenshot"]:
+                self._screenshot()
+            elif "goHome" in cmds:
+                cmds.remove("goHome")
+                self._to_home()
+                send_cmds(cmds)
+            elif "goApps" in cmds:
+                cmds.remove("goApps")
+                self._to_applications()
+                send_cmds(cmds)
+            elif "goAdmin" in cmds:
+                cmds.remove("goAdmin")
+                self._to_admin_settings_menu()
+                send_cmds(cmds)
+            elif "goReset" in cmds:
+                cmds.remove("goReset")
+                self._to_reset_settings_menu()
+                send_cmds(cmds)
+            elif cmds[0].startswith("setFont"):
+                set_font(cmds[0][-1])
+            elif cmds == ["getFontScreenshots"]:
+                for size in ["1", "2", "3", "4", "5"]:
+                    size_name = set_font(size)
+                    self._to_applications("Admin Settings")
+                    self._screenshot(full_name=f"admin_{size_name}")
+                    self.xml.send_key("KeyPad5")
+                    self._screenshot(full_name=f"reset_{size_name}")
+                    print(f"({size_name} screenshots complete!)\n")
+            elif cmds:
+                send_cmds(cmds)
+
+
+def replace_key_shortcuts(key_list: list[str]) -> list[str]:
+    shortcuts: dict = {
+        "down": "NavDwn",
+        "up": "NavUp",
+        "left": "NavLeft",
+        "right": "NavRight",
+        "select": "NavSelect",
+        "back": "NavBack",
+        "apps": "Applications",
+    }
+
+    correct_list: list[str] = []
+    for key in key_list:
+        if key in shortcuts:
+            correct_list.append(shortcuts[key])
+        elif key.isnumeric() and len(key) == 1:
+            correct_list.append(f"KeyPad{key}")
+        else:
+            correct_list.append(key)
+    return correct_list
