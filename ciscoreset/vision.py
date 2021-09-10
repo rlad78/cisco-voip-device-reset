c5e46b9d4291597c03b51278ce89d347082296cd
arf <goobysc@gmail.com>
2021-09-10 15:48:00 -0400

    handling multiple font sizes

--- ciscoreset/vision.py
+++ ciscoreset/vision.py
@@ -1,7 +1,21 @@
-from typing import Tuple
-from cv2 import imread, matchTemplate, TM_CCOEFF_NORMED
+from cv2 import (
+    imread,
+    cvtColor,
+    Canny,
+    minMaxLoc,
+    matchTemplate,
+    TM_CCOEFF_NORMED,
+    COLOR_BGR2GRAY,
+)
 import numpy as np
 from pathlib import Path
+from typing import Any, Tuple
+from collections import namedtuple
+from icecream import ic
+
+
+def imread_edges(img_path: str) -> Any:
+    return Canny(cvtColor(imread(img_path), COLOR_BGR2GRAY), 50, 200)
 
 
 def get_coords(subimage_path: str, main_image_path: str) -> Tuple[int, int]:
@@ -11,13 +25,27 @@ def get_coords(subimage_path: str, main_image_path: str) -> Tuple[int, int]:
     return np.unravel_index(result.argmax(), result.shape)
 
 
+def guess_best_coords(subimages_dir: str, main_image_path: str) -> Tuple[int, int, str]:
+    main_img = imread_edges(main_image_path)
+    Image = namedtuple("Image", ["correlation", "name", "x", "y"])
+    winner = Image(0, "", 0, 0)
+    for sub_path in [p for p in Path(subimages_dir).iterdir() if p.suffix == ".png"]:
+        sub_img = imread_edges(str(sub_path))
+        result = matchTemplate(main_img, sub_img, TM_CCOEFF_NORMED)
+        _, bestVal, _, coords = minMaxLoc(result)
+        if bestVal > winner.correlation:
+            winner = Image(bestVal, sub_path.stem, coords[0], coords[1])
+    return winner.x, winner.y, winner.name
+
+
 def get_menu_position(menu_item: str, model: str, screenshot_path: str) -> int:
     if (menu_data := MENU_SUPPORT.get(model, None)) is None:
         raise Exception(f"{model} is not supported for menu auto-navigation")
     if (icon := find_subimage_file(menu_item, "icons", model)) == "":
         raise Exception(f"{menu_item} is not a valid menu item.")
     row, col = get_coords(icon, screenshot_path)
-    # print(f"x: {col} | y: {row}")
+    ic(menu_item)
+    ic((col, row))
 
     menu_cols: tuple[int] = menu_data["coordinates"]["columns"]
     menu_rows: tuple[int] = menu_data["coordinates"]["rows"]
@@ -27,24 +55,46 @@ def get_menu_position(menu_item: str, model: str, screenshot_path: str) -> int:
             for y in range(4):
                 if row >= menu_rows[y] and row <= menu_rows[y + 1]:
                     # print(f"({x+1}, {y+1})")
-                    return (x + 1) + (4 * (y))
+                    return ic((x + 1) + (4 * (y)))
     else:
         return -1
 
 
-def get_list_position(entry: str, model: str, screenshot_path: str) -> int:
+def get_list_position(list_item: str, model: str, screenshot_path: str) -> int:
     if model not in MENU_SUPPORT:
         raise Exception(f"Cisco {model} not supported for list auto-navigation")
 
-    menu_img_file = find_subimage_file(entry, "menus", model)
-    y, x = get_coords(menu_img_file, screenshot_path)
-    # print(f"{entry}: {x=}, {y=}")
+    sub_images_dir: str = find_subimage_folder(list_item, "menus", model)
+    x, y, fontsize = guess_best_coords(sub_images_dir, screenshot_path)
 
+    # menu_img_file = find_subimage_file(entry, "menus", model)
+    # y, x = get_coords(menu_img_file, screenshot_path)
+    ic(list_item)
+    ic((x, y, fontsize))
+
+    # ic(entry)
     for i, height in enumerate(MENU_SUPPORT[model]["list_heights"]):
         if y <= height:
-            return i + 1
+            return ic(i + 1)
     else:
-        raise Exception(f"Could not find list item '{entry}'")
+        raise Exception(f"Could not find list item '{list_item}'")
+
+
+def find_subimage_folder(wanted: str, image_type: str, model: str) -> str:
+    if (menu_data := MENU_SUPPORT.get(model, None)) is None:
+        raise Exception(f"Cisco {model} does not have subimage files")
+    if (img_folder := menu_data.get(image_type, None)) is None:
+        raise Exception(f"{image_type} is not a valid image type for Cisco {model}")
+    possible_dirs: list[Path] = [
+        dir for dir in Path(img_folder).iterdir() if dir.is_dir()
+    ]
+
+    wanted_stem = wanted.lower().replace(" ", "_")
+    for folder in possible_dirs:
+        if wanted_stem == folder.stem:
+            return str(folder.resolve())
+    else:
+        return ""
 
 
 def find_subimage_file(wanted: str, image_type: str, model: str) -> str:
