bbe32f6b3d488bb81f929a74c917757094cda584
arf <goobysc@gmail.com>
2021-09-13 10:14:19 -0400

    last changes

--- reset_lg.py
+++ reset_lg.py
@@ -9,7 +9,7 @@ def reset_lines_slowly(ucm: CUCM, lines: list[Tuple[str, str]], name="") -> None
     try:
         for line in tqdm(lines, desc=name, leave=False):
             if line != lines[0]:
-                sleep(90)
+                sleep(30)
             devices: list[str] = ucm.get_dn_devices(line[0], line[1])
             for device in devices:
                 ucm.do_device_reset(name=device)
@@ -22,8 +22,14 @@ def reset_lines_slowly(ucm: CUCM, lines: list[Tuple[str, str]], name="") -> None
 
 if __name__ == "__main__":
     ucm = CUCM(*get_credentials())
-    # line_groups: dict = ucm.get_all_line_group_members()
+    line_groups: dict = ucm.get_all_line_group_members()
     group_sample = [
+        "Redfern COVID-19 Group 5XX",
+        "Redfern COVID-19 Group 5XX - 6XX",
+        "Redfern COVID-19 Group 6XX",
+        "Redfern COVID-19 Group 7XX",
+        "Redfern COVID-19 Group ALL",
+        "IPTAY - Sales and Service LG",
         "Facilities-Dispatch-LG",
         "Redfern COVID-19 Group ALL",
         "Redfern Pharmacy Group",
@@ -32,12 +38,17 @@ if __name__ == "__main__":
         "Sullivan-COVID-19-LG",
         "Undergrad-LG",
     ]
-    # group_sample = [f"hunt-lab{i}" for i in range(1, 6, 1)]
-    line_groups: dict = {
-        name: ucm.get_line_group_members(name) for name in group_sample
-    }
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=7) as ex:
+    for name in group_sample:
+        line_groups.pop(name, None)
+
+    # print(list(line_groups.keys()))
+    # group_sample = [f"hunt-lab{i}" for i in range(1, 6, 1)]
+    # line_groups: dict = {
+    #     name: ucm.get_line_group_members(name) for name in group_sample
+    # }
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as ex:
         lg_futures = [
             ex.submit(reset_lines_slowly, ucm, lg_members, lg_name)
             for lg_name, lg_members in line_groups.items()
