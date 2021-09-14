296ca01ddd83efcb6336e5ac49d171e99785bd4c
arf <goobysc@gmail.com>
2021-09-14 16:17:39 -0400

    ucm/axl validation + broken up crdentials funcs

--- ciscoreset/credentials.py
+++ ciscoreset/credentials.py
@@ -1,37 +1,132 @@
 from pathlib import Path
+from requests.adapters import ConnectTimeout, ConnectionError
 from stdiomask import getpass
 from cryptography.fernet import Fernet, InvalidToken
+import requests
 import json
 from typing import Tuple
+import tldextract
+import validators
+from urllib.parse import urlparse
 
 
-def get_credentials() -> Tuple[str, str]:
-    key_loc = Path().cwd().parent / "ciscoxml_passkey"
-    stored = Path("pass.log")
+KEY_LOC = Path().cwd().parent / "ciscoreset_passkey"
+CREDS = Path("pass.log")
 
-    if stored.is_file() and key_loc.is_file():
+
+def get_credentials(enable_manual_entry=True) -> Tuple[str, str]:
+    # key_loc = Path().cwd().parent / "ciscoreset_passkey"
+    # stored = Path("pass.log")
+
+    if CREDS.is_file() and KEY_LOC.is_file():
         try:
-            with key_loc.open("rb") as k:
-                fernet = Fernet(k.read())
-            with stored.open("rb") as f:
-                d = json.loads(fernet.decrypt(f.read()).decode())
-            print("Using stored encrypted passwords from", str(stored.resolve()), "\n")
+            d = credentials_from_file()
         except InvalidToken:
-            key_loc.unlink()
-            stored.unlink()
+            KEY_LOC.unlink()
+            CREDS.unlink()
             return get_credentials()
+    elif enable_manual_entry:
+        d = credentials_from_input()
     else:
-        d: dict = {
-            "user": input("CUCM username: "),
-            "pass": getpass(prompt="CUCM password: "),
-            # "tacacs": getpass(prompt="TACACS Password: "),
-            # "bang": getpass(prompt='"bang" Password: '),
-        }
-        key = Fernet.generate_key()
-        fernet = Fernet(key)
-        with stored.open("wb") as f:
-            f.write(fernet.encrypt(json.dumps(d).encode()))
-        with key_loc.open("wb") as k:
-            k.write(key)
-        print("Writing ENCRYPTED passwords to", str(stored.resolve()))
-    return d["user"], d["pass"]
+        return "", ""
+    return d["username"], d["password"]
+
+
+def credentials_from_file() -> dict:
+    with KEY_LOC.open("rb") as k:
+        fernet = Fernet(k.read())
+    with CREDS.open("rb") as f:
+        d = json.loads(fernet.decrypt(f.read()).decode())
+    print("Using stored encrypted passwords from", str(CREDS.resolve()), "\n")
+    return d
+
+
+def credentials_from_input() -> dict:
+    d: dict = {
+        "username": input("CUCM username: "),
+        "password": getpass(prompt="CUCM password: "),
+        # "tacacs": getpass(prompt="TACACS Password: "),
+        # "bang": getpass(prompt='"bang" Password: '),
+    }
+    write_credentials(**d)
+    print("Writing ENCRYPTED passwords to", str(CREDS.resolve()))
+    return d
+
+
+def write_credentials(username: str, password: str) -> None:
+    d = {"username": username, "password": password}
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    with CREDS.open("wb") as f:
+        f.write(fernet.encrypt(json.dumps(d).encode()))
+    with KEY_LOC.open("wb") as k:
+        k.write(key)
+    print("Writing ENCRYPTED passwords to", str(CREDS.resolve()))
+
+
+def get_url_status_code(url: str, username="", password="", timeout=10) -> int:
+    if all((username, password)):
+        auth = requests.auth.HTTPBasicAuth(username, password)
+    else:
+        auth = None
+
+    try:
+        return requests.get(url, auth=auth, timeout=timeout).status_code
+    except ConnectionError:
+        return -1
+    except ConnectTimeout:
+        return 0
+
+
+def validate_ucm_server(url: str, port: int) -> str:
+    fullurl = generate_proper_url(url, port)
+    if not validators.url(fullurl):
+        return f"{fullurl} is not a valid URL"
+
+    status = get_url_status_code(fullurl, timeout=10)
+    if status == 200:
+        return ""
+    elif status == -1:
+        return f"Could not find {url}, please check URL"
+    elif status == 0:
+        return "Could not connect, please make sure port and URL are correct"
+
+
+def validate_axl_auth(ucm: str, port: int, username: str, password: str) -> bool:
+    fullurl: str = generate_proper_url(ucm, port)
+    if not all((username, password)):
+        return False
+
+    if fullurl.endswith("/"):
+        fullurl += "axl/"
+    else:
+        fullurl += "/axl/"
+
+    if not validators.url(fullurl):
+        raise Exception(f"{fullurl} is not a valid URL")
+
+    status = get_url_status_code(fullurl, username, password, timeout=3)
+    if status != 200:
+        return False
+    else:
+        return True
+
+
+def generate_proper_url(url: str, port=0) -> str:
+    if not url.startswith("http://") and not url.startswith("https://"):
+        url = "https://" + url
+
+    url_parts = urlparse(url)
+    scheme = url_parts.scheme
+    netloc = url_parts.netloc
+    urlpath = url_parts.path
+
+    # subdomain, domain, suffix = tldextract(url)
+    if port == 0:
+        return f"{scheme}://{netloc}{urlpath}"
+    else:
+        return f"{scheme}://{netloc}:{port}{urlpath}"
+
+
+def get_base_url(url: str) -> str:
+    return ".".join(tldextract.extract(generate_proper_url(url)))
