e1cb45773c5265dcf613da0acb8da85ae5739918
arf <goobysc@gmail.com>
2021-09-15 15:10:39 -0400

    fixed some dl issues with headers

--- ciscoreset/xml.py
+++ ciscoreset/xml.py
@@ -83,6 +83,9 @@ class XMLPhone:
             resp = requests.get(
                 f"http://{self.ip}/CGI/Screenshot",
                 auth=requests.auth.HTTPBasicAuth(self.username, self.password),
+                headers={
+                    "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
+                },
             )
 
             if not resp.ok:
