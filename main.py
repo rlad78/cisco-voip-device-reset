e60e5b356fbda684a67d8f6503c80bee42783bce
arf <goobysc@gmail.com>
2021-09-15 11:13:06 -0400

    make executable with venv

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#!.venv/bin/python3
 from time import sleep
 from ciscoreset import PhoneConnection, get_menu_position, get_list_position
 import ciscoreset.gui_homepage as gui
 
 
 # with PhoneConnection("10.12.4.231", verbose=True) as myphone:
-#     myphone._screenshot(full_name="screenshot.bmp")
+#     myphone._screenshot(full_name="screenshot")
 
 gui.run()
