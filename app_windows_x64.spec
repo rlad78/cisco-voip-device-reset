d5dd07cf4e9dcbceab7c849d8127f61e08f358e7
rlad78 <goobysc@gmail.com>
2021-09-21 20:17:18 -0400

    get version through poetry

--- app_windows_x64.spec
+++ app_windows_x64.spec
@@ -1,7 +1,7 @@
 # -*- mode: python ; coding: utf-8 -*-
 from PyInstaller.utils.hooks import collect_data_files
 
-datas = []
+datas = [('pyproject.toml', '.')]
 datas += collect_data_files('ciscoreset')
 datas += collect_data_files('ciscoaxl')
 
--- ciscoreset/__init__.py
+++ ciscoreset/__init__.py
@@ -1,7 +1,13 @@
-__version__ = "0.1.0"
+import toml
+
 from .axl import CUCM
 from .credentials import get_credentials
 from .keys import KEY_SUPPORT
 from .vision import get_menu_position, get_list_position
 from .xml import XMLPhone
 from .phone import PhoneConnection
+from .configs import ROOT_DIR
+
+__version__ = toml.load(str(ROOT_DIR.parent / "pyproject.toml"))["tool"]["poetry"][
+    "version"
+]
