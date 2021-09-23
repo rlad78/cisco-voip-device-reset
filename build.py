3da3a53d2e555ccebbd906b7c5770a56e026543a
rlad78 <goobysc@gmail.com>
2021-09-23 18:07:08 -0400

    can detect Rosetta 2 on M1 Mac

--- build.py
+++ build.py
@@ -1,29 +1,48 @@
 import PyInstaller.__main__
 from pathlib import Path
+import platform
 
 
-def __base(arch: str, target_os: str):
+def __base(target_os: str):
     PyInstaller.__main__.run(
         [
             f"app_{target_os}.spec",
             "--clean",
             "--noconfirm",
             f"-p {str(Path(__file__).parent)}",
-            # f"--target-architecture {arch}" if target_os == "mac" else "",
         ]
     )
 
 
 def mac_m1():
-    __base("arm64", "mac")
+    print("Building for macOS (arm64)")
+    __base("mac_m1")
 
 
 def mac():
-    __base("x86_64", "mac")
+    print("Building for macOS (Intel)")
+    __base("mac")
 
 
 def windows():
-    __base("", "windows")
+    print("Building for Windows")
+    __base("windows")
+
+
+def auto():
+    sys_os = platform.system()
+    sys_arch = platform.processor()
+
+    if sys_os == "Darwin" and sys_arch == "arm":
+        mac_m1()
+    elif sys_os == "Darwin" and sys_arch == "i386":
+        mac()
+    elif sys_os == "Windows":
+        windows()
+    elif sys_os == "Linux":
+        print("Sorry, Linux is not yet supported")
+    else:
+        print(f"Unknown OS type: {sys_os}{'_' + sys_arch if sys_arch else ''}")
 
 
 # pathex=[str(Path(__file__).parent)],
